c735f27ee240133ae344c04f66dd1e6ae5ab0a2d
AsliAliyeva <eslieliyeva01@mail.ru>
2024-11-22 04:57:40 +0400

    blog css finish

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import Header from './companent/Header/Header'
 import Footer from './companent/Footer/Footer'
 import Veramar from './companent/Main/Veramar'
+import Blog from './companent/Main/Blog'
 
 function App() {
   return (
     <>
       <Header />
-      <Veramar />
+      <Blog />
       <Footer />
     </>
   )
